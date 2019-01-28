0656a38b7379a40469f93dd1ddc6d3459d316819
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 18:06:33 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -19,7 +19,7 @@ class Timer extends Component {
     this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
   }
 
-  ShouldComponentUpdate(){
+  shouldComponentUpdate(){
     if (this.state.time === nextState.time) {
       return false
     }
